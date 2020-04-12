0e740c9c7367fc7aa5bdf43264b12816d80993b6
Alexander Low <alexanderlow@MacBook-Air.local>
2020-04-12 14:56:53 -0400

    fix: changed gitignore and sub function

--- src/functions/subscribe.js
+++ src/functions/subscribe.js
@@ -14,9 +14,6 @@ exports.handler = function(event, context, callback) {
   const send = body => {
     callback(null, {
       statusCode: 200,
-      headers: {
-        "Content-Type": "application/json",
-      },
       body: JSON.stringify(body),
     })
   }
