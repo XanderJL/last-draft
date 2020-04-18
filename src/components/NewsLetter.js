872b1d02059f96370d1cd694d8dca69f2bb06521
Alexander Low <alexanderlow@MacBook-Air.local>
2020-04-18 17:13:57 -0400

    Added confirmation modal to newsletter

--- src/components/NewsLetter.js
+++ src/components/NewsLetter.js
@@ -3,12 +3,15 @@ import BackgroundImage from "gatsby-background-image"
 import Img from "gatsby-image"
 import axios from "axios"
 
+import Modal from "../components/Modal"
+
 export class NewsLetter extends Component {
   constructor(props) {
     super(props)
     this.state = {
       first_name: "",
       email: "",
+      setModal: false,
     }
   }
 
@@ -28,66 +31,74 @@ export class NewsLetter extends Component {
       email: this.state.email,
     })
 
-    this.setState({ first_name: "", email: "" })
+    this.setState({ first_name: "", email: "", setModal: true })
 
     e.preventDefault()
   }
 
   render() {
     return (
-      <BackgroundImage
-        className="hero-newsletter is-fullheight"
-        fluid={this.props.image}
-        style={{ backgroundAttachment: "fixed" }}
-      >
-        <Img
+      <>
+        <BackgroundImage
+          className="hero-newsletter is-fullheight"
           fluid={this.props.image}
-          className="is-hidden-desktop"
-          objectFit="contain"
-          alt="Image of a typewriter"
-        />
-        <div className="hero-body">
-          <div className="container">
-            <form onSubmit={this.handleSubmit}>
-              <div className="field">
-                <h1 className="title is-montserrat has-text-white">
-                  Sign Up For Our Newsletter
-                </h1>
-              </div>
-              <div className="field">
-                <input
-                  type="text"
-                  name="first_name"
-                  value={this.state.first_name}
-                  onChange={this.handleChange}
-                  className="input"
-                  placeholder="First Name"
-                />
-              </div>
-              <div className="field">
-                <input
-                  type="text"
-                  name="email"
-                  value={this.state.email}
-                  onChange={this.handleChange}
-                  className="input"
-                  placeholder="Email"
-                />
-              </div>
-              <div className="field">
-                <div className="control">
-                  <button
-                    type="submit"
-                    className="button is-white is-montserrat is-uppercase has-text-weight-bold"
-                  >
-                    submit
-                  </button>
+          style={{ backgroundAttachment: "fixed" }}
+        >
+          <Img
+            fluid={this.props.image}
+            className="is-hidden-desktop"
+            objectFit="contain"
+            alt="Image of a typewriter"
+          />
+          <div className="hero-body">
+            <div className="container">
+              <form onSubmit={this.handleSubmit}>
+                <div className="field">
+                  <h1 className="title is-montserrat has-text-white">
+                    Sign Up For Our Newsletter
+                  </h1>
                 </div>
-              </div>
-            </form>
+                <div className="field">
+                  <input
+                    type="text"
+                    name="first_name"
+                    value={this.state.first_name}
+                    onChange={this.handleChange}
+                    className="input"
+                    placeholder="First Name"
+                  />
+                </div>
+                <div className="field">
+                  <input
+                    type="text"
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.handleChange}
+                    className="input"
+                    placeholder="Email"
+                  />
+                </div>
+                <div className="field">
+                  <div className="control">
+                    <button
+                      type="submit"
+                      className="button is-white is-montserrat is-uppercase has-text-weight-bold"
+                    >
+                      submit
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
           </div>
-        </div>
-      </BackgroundImage>
+        </BackgroundImage>
+        {this.state.setModal ? (
+          <Modal
+            header="Thank You!"
+            body="Please check your inbox to confirm your email address."
+          />
+        ) : null}
+      </>
     )
   }
 }
