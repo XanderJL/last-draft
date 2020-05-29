5bdf272ce0d68a6de979da638e99e619eeb3b8b3
XanderJL <alexanderjameslow@gmail.com>
2020-05-28 20:56:14 -0400

    removed: legacy blocks

--- src/components/TestimonialCarousel.js
+++ src/components/TestimonialCarousel.js
@@ -93,55 +93,6 @@ export default function Testimonials() {
             </a>
           </div>
         ))}
-        {/* <div className="quote">
-          <p className="has-text-centered has-text-weight-light is-italic is-size-5">
-            "Working with Last Draft over the past three years has been
-            invaluable to CEO Quest. The content strategies they executed
-            expanded our reach and helped us form several strategic
-            partnerships. Their superior writing skills helped bolster our
-            position as company-building experts."
-          </p>
-          <div className="client has-text-centered is-size-5">
-            <p>Tom Mohr</p>
-            <p>Founder, CEO Quest</p>
-          </div>
-          <a
-            href="http://ceoquest.com"
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            <Img
-              className="testimonial-logo"
-              fluid={ceoLogo}
-              alt="CEO Quest logo"
-              objectFit="contain"
-            />
-          </a>
-        </div>
-        <div className="quote">
-          <p className="has-text-centered has-text-weight-light is-italic is-size-5">
-            “Last Draft has been an invaluable partner in helping us develop our
-            brand positioning and content marketing strategies. They take the
-            time to listen, understand and research our point of view, resulting
-            in high quality content that’s written in our voice.”
-          </p>
-          <div className="client has-text-centered is-size-5">
-            <p>Bob Bausmith</p>
-            <p>Founder, Accelerate Innovation</p>
-          </div>
-          <a
-            href="https://www.accelerateinnovation.com/"
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            <Img
-              className="testimonial-logo"
-              fluid={accelerateLogo}
-              alt="Accelerate Innovation logo"
-              objectFit="contain"
-            />
-          </a>
-        </div> */}
       </Slider>
     </section>
   )
