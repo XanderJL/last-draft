8b6d67e73ec35ccde11998613dcf6a366c9dd66d
XanderJL <alexanderjameslow@gmail.com>
2020-06-24 21:13:02 -0400

    added: createPage for blog posts and categories

--- gatsby-node.js
+++ gatsby-node.js
@@ -19,6 +19,18 @@ exports.createPages = async ({ graphql, actions }) => {
           }
         }
       }
+      categories: allSanityCategory(
+        filter: { slug: { current: { ne: null } } }
+      ) {
+        edges {
+          node {
+            slug {
+              current
+            }
+            title
+          }
+        }
+      }
     }
   `)
 
@@ -27,6 +39,7 @@ exports.createPages = async ({ graphql, actions }) => {
   }
 
   const posts = res.data.posts.edges
+  const categories = res.data.categories.edges
 
   posts.forEach(edge => {
     const path = edge.node.category
@@ -37,6 +50,16 @@ exports.createPages = async ({ graphql, actions }) => {
       path,
       component: require.resolve("./src/templates/post.js"),
       context: { slug: edge.node.slug.current },
+    })
+  })
+
+  categories.forEach(edge => {
+    const path = `/the-last-draft/${edge.node.slug.current}`
+
+    createPage({
+      path,
+      component: require.resolve("./src/templates/category.js"),
+      context: { slug: edge.node.slug.current, title: edge.node.title },
     })
   })
 }
