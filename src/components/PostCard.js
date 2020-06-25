cd7ac646eced9206dd52b4536b5cb665fefb6778
XanderJL <alexanderjameslow@gmail.com>
2020-06-25 15:06:29 -0400

    fixed: title font size for mobile

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -7,7 +7,7 @@ const PostCard = props => {
     <div className="card">
       <div className="card-image">{fluid ? <Img fluid={fluid} /> : null}</div>
       <div className="card-content">
-        <h2 className="title is-size-2-desktop is-montserrat is-uppercase has-text-black">
+        <h2 className="title is-size-2-desktop is-size-4-mobile is-montserrat is-uppercase has-text-black">
           {title}
         </h2>
         {children}
