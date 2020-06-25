5fee732cec45d49d99ea52f4ea45156024988086
XanderJL <alexanderjameslow@gmail.com>
2020-06-24 21:13:20 -0400

    refactored: added min-height for hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,10 +2,10 @@ import React from "react"
 import PropTypes from "prop-types"
 import BackgroundImage from "gatsby-background-image"
 
-const Hero = ({ children, fluid }) => {
+const Hero = ({ children, fluid, styles }) => {
   return fluid ? (
-    <BackgroundImage className="hero is-large" fluid={fluid}>
-      <div className="hero-body">
+    <BackgroundImage className="hero is-large" fluid={fluid} style={styles}>
+      <div className="hero-body" style={{ minHeight: "200px" }}>
         <div className="container">{children}</div>
       </div>
     </BackgroundImage>
