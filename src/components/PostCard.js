4099c9c51b0eb3fd47492437aa94bbe48b3d7676
XanderJL <alexanderjameslow@gmail.com>
2020-06-26 15:09:12 -0400

    feature: post card component

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,19 +1,36 @@
 import React from "react"
+import PropTypes from "prop-types"
+import { Link } from "gatsby"
 import Img from "gatsby-image"
 
 const PostCard = props => {
-  const { title, fluid, children } = props
+  const { title, image, children, link } = props
   return (
     <div className="card">
-      <div className="card-image">{fluid ? <Img fluid={fluid} /> : null}</div>
+      <div className="card-image">
+        {image ? (
+          <Link to={link}>
+            <Img className="post-image" fluid={image} />
+          </Link>
+        ) : null}
+      </div>
       <div className="card-content">
-        <h2 className="title is-size-2-desktop is-size-4-mobile is-montserrat is-uppercase has-text-black">
+        <h2 className="title is-size-3-desktop is-size-4-mobile is-montserrat is-uppercase has-text-black">
           {title}
         </h2>
-        {children}
+        <div className="content">{children}</div>
+        <Link to={link} className="button">
+          Read More &rsaquo;
+        </Link>
       </div>
     </div>
   )
+}
+
+PostCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  image: PropTypes.object.isRequired,
+  link: PropTypes.string.isRequired,
 }
 
 export default PostCard
