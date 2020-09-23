beae91eaa84db29402c40df986f636771e4ad6d6
XanderJL <alexanderjameslow@gmail.com>
2020-09-23 14:19:03 -0400

    added: new singleton pages

--- sanity/src/deskStructure/deskStructure.js
+++ sanity/src/deskStructure/deskStructure.js
@@ -1,27 +1,7 @@
-import S, { listItem } from "@sanity/desk-tool/structure-builder";
+import S from "@sanity/desk-tool/structure-builder";
 import { AiFillHome, AiFillEdit } from "react-icons/ai";
 import { BsInfoCircleFill } from "react-icons/bs";
 import { RiServiceLine } from "react-icons/ri";
-
-// // Simple example of web preview
-// const url =
-//   "https://www.gatsbyjs.com/dashboard/7a0e3543-160d-45fe-8e0e-eaa310ee3062/sites/d60554fe-9f49-42ad-af3d-167a43454cda/production";
-// const WebPreview = ({ document }) => {
-//   const { displayed } = document;
-//   return <iframe src={url + displayed.slug.current} frameBorder={0} />;
-// };
-
-// export const getDefaultDocumentNode = ({ schemaType }) => {
-//   // Conditionally return a different configuration based on the schema type
-//   if (schemaType === "project") {
-//     return S.document().views([
-//       S.view.form(),
-//       S.view.component(WebPreview).title("Web"),
-//     ]);
-//   }
-// };
-
-// export default S.defaults();
 
 const hiddenDocTypes = (listItem) =>
   !["aboutPage", "indexPage", "servicesPage", "blog"].includes(
@@ -51,19 +31,33 @@ export default () =>
             .documentId("aboutPage")
         ),
       S.listItem()
-        .title("Services Page")
+        .title("Entrepreneur Services Page")
         .icon(RiServiceLine)
         .child(
           S.editor()
-            .id("sservicesPage")
+            .id("entrepreneurServicesPage")
             .schemaType("servicesPage")
-            .documentId("servicesPage")
+            .documentId("entrepreneurServicesPage")
+        ),
+      S.listItem()
+        .title("Artist Services Page")
+        .icon(RiServiceLine)
+        .child(
+          S.editor()
+            .id("artistServicesPage")
+            .schemaType("servicesPage")
+            .documentId("artistServicesPage")
         ),
       S.divider(),
       S.listItem()
         .title("Blog")
         .icon(AiFillEdit)
-        .child(S.editor().id("blog").schemaType("blog").documentId("blog")),
+        .child(
+          S.editor()
+            .id("blog")
+            .schemaType("blog")
+            .documentId("blog")
+        ),
       S.divider(),
       ...S.documentTypeListItems().filter(hiddenDocTypes),
     ]);
