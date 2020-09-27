2b353c575a741109b48482235305f5e15771e21c
XanderJL <alexanderjameslow@gmail.com>
2020-09-26 22:28:11 -0400

    changed: switched out components for rendering convertkit form

--- site/src/components/ConvertKitModal.js
+++ site/src/components/ConvertKitModal.js
@@ -1,16 +1,15 @@
 import React from "react"
-import ConvertKitForm from "convertkit-react"
+import ScriptTag from "react-script-tag"
 import Modal from "./Modal"
-
-const formConfig = {
-  formId: 1301591,
-  template: "Charlotte",
-}
 
 const ConvertKitModal = () => {
   return (
     <Modal>
-      <ConvertKitForm {...formConfig} />
+      <ScriptTag
+        async
+        data-uid="348b0dcb03"
+        src="https://motivated-speaker-4381.ck.page/348b0dcb03/index.js"
+      ></ScriptTag>
     </Modal>
   )
 }
